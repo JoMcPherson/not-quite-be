5ed6c19727380001e2a47bda71212a23f6f7d03f
McPherson <joanna.mcpherson@cvent.com>
2024-08-12 10:32:27 -0400

    first commit of new repo

---
+++ src/test/java/com/notquite/notquiteservice/NotquiteserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notquite.notquiteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotquiteserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
