dc3631c0b11661ca02f14f35ac7ccf45c8ba369f
McPherson <joanna.mcpherson@cvent.com>
2024-08-12 12:51:52 -0400

    updating events structure

--- src/main/java/com/notquite/notquiteservice/models/Event.java
+++ src/main/java/com/notquite/notquiteservice/models/Event.java
@@ -1,31 +1,134 @@
 package com.notquite.notquiteservice.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
 
 
 @Entity
+@Table(name = "events")
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
-    public Event() {
+    private String title;
 
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private String location;
 
-    public Long getId() {
+
+    private String description;
+
+
+    private String cognitoUserId;
+
+    private LocalDateTime date;
+
+
+    private String image;
+
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+
+    private Boolean cancelled = false;
+
+    private Integer maxAttendees;
+
+    // Getters and Setters
+
+    public Integer getId() {
         return id;
     }
 
-    public Event(Long id) {
+    public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCognitoUserId() {
+        return cognitoUserId;
+    }
+
+    public void setCognitoUserId(String cognitoUserId) {
+        this.cognitoUserId = cognitoUserId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(LocalDateTime lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public Boolean getCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(Boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    public Integer getMaxAttendees() {
+        return maxAttendees;
+    }
+
+    public void setMaxAttendees(Integer maxAttendees) {
+        this.maxAttendees = maxAttendees;
     }
 }
