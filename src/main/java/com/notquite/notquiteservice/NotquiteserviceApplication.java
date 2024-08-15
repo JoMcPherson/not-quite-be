cb094971e8ee5766b4f3624cb9e72a2cb5140c9e
Juan.Pinol <juan.pinol@cvent.com>
2024-08-15 16:01:08 -0400

    added a scheduled console log as example and emailSending class

--- src/main/java/com/notquite/notquiteservice/NotquiteserviceApplication.java
+++ src/main/java/com/notquite/notquiteservice/NotquiteserviceApplication.java
@@ -1,19 +1,24 @@
 package com.notquite.notquiteservice;
 
 import com.notquite.notquiteservice.filter.CustomCorsFilter;
+import com.notquite.notquiteservice.services.ScheduledTasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class NotquiteserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotquiteserviceApplication.class, args);
 	}
+
+	ScheduledTasks scheduledTasks;
 
 	@Bean
 	public FilterRegistrationBean<CustomCorsFilter> corsFilterRegistrationBean() {
