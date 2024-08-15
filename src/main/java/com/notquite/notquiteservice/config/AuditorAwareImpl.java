1e1e7d22b2fbf05a7069ee03d29acdceb3edc011
McPherson <joanna.mcpherson@cvent.com>
2024-08-15 14:59:59 -0400

    attendee updates

--- src/main/java/com/notquite/notquiteservice/config/AuditorAwareImpl.java
+++ src/main/java/com/notquite/notquiteservice/config/AuditorAwareImpl.java
@@ -1,13 +1,17 @@
 package com.notquite.notquiteservice.config;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
 public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
+    @NonNull
     public Optional<String> getCurrentAuditor() {
         Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String userId = jwt.getClaimAsString("sub");
