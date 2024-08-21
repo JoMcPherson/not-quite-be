31ee9d563beaec88c163b052f6b6b0bd57ef7e4a
Juan.Pinol <juan.pinol@cvent.com>
2024-08-21 12:14:48 -0400

    finished a scheduled email notification that runs every morning

--- src/main/java/com/notquite/notquiteservice/config/SecurityConfig.java
+++ src/main/java/com/notquite/notquiteservice/config/SecurityConfig.java
@@ -1,10 +1,7 @@
 package com.notquite.notquiteservice.config;
 
 import org.springframework.context.annotation.Bean;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
--- src/main/java/com/notquite/notquiteservice/services/ScheduledTasks.java
+++ src/main/java/com/notquite/notquiteservice/services/ScheduledTasks.java
@@ -1,31 +1,59 @@
 package com.notquite.notquiteservice.services;
 
-import com.notquite.notquiteservice.models.Event;
+import com.amazonaws.services.cognitoidp.model.UserType;
+import com.notquite.notquiteservice.models.EventAttendee;
 import com.notquite.notquiteservice.models.dto.EventDTO;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 @Component
 public class ScheduledTasks {
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
     private final EventService eventService;
+    private final CognitoService cognitoService;
+    private final EventAttendeeService eventAttendeeService;
+    private final EmailService emailService;
 
-    public ScheduledTasks(EventService eventService) {
+    public ScheduledTasks(EventService eventService, CognitoService cognitoService,
+            EventAttendeeService eventAttendeeService, EmailService emailService) {
         this.eventService = eventService;
+        this.cognitoService = cognitoService;
+        this.eventAttendeeService = eventAttendeeService;
+        this.emailService = emailService;
     }
 
-    // runs every 12 hours~
-    @Scheduled(fixedRate = 43200000)
+    // runs every day at 6AM
+    @Scheduled(cron = "0 30 6 * * *")
     public void reportCurrentTimeAndTomorrowsEvents() {
         LocalDateTime today = LocalDateTime.now();
-        System.out.println("The time is now: " + today.toString());
-        List<EventDTO> events = this.eventService.getAllEventsBetweenDates(today.plusDays(1), today.plusDays(2));
-        events.forEach(EventDTO -> System.out.println(EventDTO.getTitle()));
-    }
 
+        List<EventDTO> events = this.eventService.getAllEventsBetweenDates(today, today.plusDays(1));
+
+        List<UserType> allUsers = cognitoService.getAllUsers();
+
+        events.forEach(event -> {
+                    Long eventId = Long.valueOf(event.getId());
+
+                    List<EventAttendee> attendees = eventAttendeeService.getAllEventAttendeesByEventId(eventId);
+
+                    attendees.forEach(eventAttendee -> {
+                        allUsers.forEach(user -> {
+                            if(eventAttendee.getCognitoUserId().equals(user.getAttributes().get(3).getValue())){
+                                String email = (user.getAttributes().get(0).getValue());
+                                String name = (user.getUsername());
+                                String eventName = event.getTitle();
+                                String sport = event.getSport();
+                                String subject = "Don't forget that the ".concat(eventName).concat(" is happening " +
+                                        "today!");
+                                String emailBody =
+                                        "Hey " + name + " ,get ready to have fun doing " + sport + "!";
+                                emailService.sendEmail(email, subject, emailBody);
+                            }
+                        });
+                    });
+                }
+        );
+    }
 }
